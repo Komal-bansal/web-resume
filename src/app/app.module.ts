9871f8a1bac3e4dbb98549523e3ebd43351fcfbc
komal-bansal <komalb1408@gmail.com>
2017-09-19 20:05:43 +0530

    minor chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
@@ -41,6 +42,7 @@ export const appRoutes:Routes=[
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true }
