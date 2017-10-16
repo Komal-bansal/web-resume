4a319492f611f42d9e4e6cfab9258a805506d152
komal-bansal <komalb1408@gmail.com>
2017-10-16 20:59:58 +0530

    resume changes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
-
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 @Component({
     selector:'portfolio',
     templateUrl:'./portfolio.component.html',
-    styleUrls:['./portfolio.component.css','../home/home.component.css']
+    styleUrls:['./portfolio.component.css','../home/home.component.css'],
+  //   animations: [
+  // trigger(
+  //     'myAnimation',
+  //     [
+  //       transition(
+  //       ':enter', [
+  //         style({transform: 'translateX(-100%)', opacity: 0}),
+  //         animate('500ms', style({transform: 'translateX(0)', 'opacity': 1}))
+  //       ]
+  //     ),
+  //     transition(
+  //       ':leave', [
+  //         style({transform: 'translateX(-0%)', 'opacity': 1}),
+  //         animate('500ms', style({transform: 'translateX(100%)', 'opacity': 0})),
+          
+  //       ]
+  //     )]
+  //   )
+  // ]
 })
 
 export class PortfolioComponent{
-
+    // show:boolean=false;
 }
